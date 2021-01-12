d1d9c63351167748580cc4ab04299c4b3cff9825
atharvsinha <45174189+atharvsinha@users.noreply.github.com>
2021-01-12 16:18:26 +0530

    Update pitch_detection_wav.py

--- pitch_detection_wav.py
+++ pitch_detection_wav.py
@@ -7,7 +7,7 @@ y, sr = librosa.load(filename)
 window = sr//2 # fft window size
 step =  window//2 # step size for fft window
 for c in range(0, len(y), step):
-  i  = int(np.argmax(abs(np.fft.fft(y[c:c+step])[:step])))
+  i  = int(np.argmax(abs(np.fft.rfft(y[c:c+step])[:step])))
   try:
     notes.append(librosa.hz_to_note(i))
   except:
